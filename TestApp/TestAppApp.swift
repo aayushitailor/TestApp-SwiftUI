9d261df0c89d98587049b01d12f2fd3b25e7633a
aayushitailor <aayushitailor1995@gmail.com>
2024-02-13 18:44:19 +0000

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Aayushi Tailor on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
